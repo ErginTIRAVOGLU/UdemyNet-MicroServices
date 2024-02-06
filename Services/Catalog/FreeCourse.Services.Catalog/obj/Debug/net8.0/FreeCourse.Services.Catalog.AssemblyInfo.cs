1f68c7cb903db41167c08f32266a923fbe3139d6
Ergin <ergintiravoglu@msn.com>
2024-02-06 11:49:00 +0300

    merged

--- Services/Catalog/FreeCourse.Services.Catalog/obj/Debug/net8.0/FreeCourse.Services.Catalog.AssemblyInfo.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/obj/Debug/net8.0/FreeCourse.Services.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd31e163298e3f139a77b778c0205f26dd70a4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c931896ca5b8170aeeeadb6ec7bbadab7c0db13f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Shared/FreeCourse.Shared/obj/Debug/net8.0/FreeCourse.Shared.AssemblyInfo.cs
+++ Shared/FreeCourse.Shared/obj/Debug/net8.0/FreeCourse.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreeCourse.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd31e163298e3f139a77b778c0205f26dd70a4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c931896ca5b8170aeeeadb6ec7bbadab7c0db13f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreeCourse.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreeCourse.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
