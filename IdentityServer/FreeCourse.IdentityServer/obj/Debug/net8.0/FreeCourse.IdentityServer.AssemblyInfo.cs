c931896ca5b8170aeeeadb6ec7bbadab7c0db13f
Ergin <ergintiravoglu@msn.com>
2024-02-06 11:44:16 +0300

    for pull

--- IdentityServer/FreeCourse.IdentityServer/obj/Debug/net8.0/FreeCourse.IdentityServer.AssemblyInfo.cs
+++ IdentityServer/FreeCourse.IdentityServer/obj/Debug/net8.0/FreeCourse.IdentityServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreeCourse.IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd31e163298e3f139a77b778c0205f26dd70a4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreeCourse.IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreeCourse.IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
