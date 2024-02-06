6bc89ef3024cb5b5b17b04a15f3271caee073837
Ergin <ergintiravoglu@msn.com>
2024-02-06 11:50:05 +0300

    merged

--- Shared/FreeCourse.Shared/obj/Debug/net8.0/FreeCourse.Shared.AssemblyInfo.cs
+++ Shared/FreeCourse.Shared/obj/Debug/net8.0/FreeCourse.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreeCourse.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c931896ca5b8170aeeeadb6ec7bbadab7c0db13f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f68c7cb903db41167c08f32266a923fbe3139d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreeCourse.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreeCourse.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
